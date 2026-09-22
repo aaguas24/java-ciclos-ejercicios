08fc8b6a23cc0d59dc2e4eb4684891a6101a2c08
aaguas24 <andresaguas360@hotmail.com>
2026-09-22 09:52:36 -0500

    Crear estructura básica de Spring Boot con Maven y clase principal

---
+++ src/main/java/com/example/ciclos/CiclosApplication.java
@@ -0,0 +1,12 @@
+package com.example.ciclos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiclosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiclosApplication.class, args);
+    }
+}
